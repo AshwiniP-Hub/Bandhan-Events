8aa33821b408e44d6638dbc476aaee473877480b
AshwiniP-Hub <ashwinivpatil4@gmail.com>
2025-01-03 12:06:07 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,28 @@
 
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  // plugins: [
-  //   require('daisyui'),
-  import plugin from 'daisyui';
-  ],
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('daisyui'),
  
-}
+//   ],
+ 
+// }
+import plugin from 'daisyui';
+
+export default {
+   content: [
+     "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+   ],
+    theme: {
+        extend: {},
+    },
+    plugins: [plugin],
+};
